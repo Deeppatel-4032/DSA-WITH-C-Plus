b40532ab3dd64dcb8eee4697dd99207242fb6e32
Deeppatel-4032 <deeppatel7868@gmail.com>
2024-09-23 19:48:25 +0530

    hello print

---
+++ hello.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int a;
+    int b;
+    int c;
+
+    cout <<"Enter the value a :";
+    cin >> a;
+
+    cout <<"Enter the value b :";
+    cin >> b;
+
+    c = a + b;
+
+    cout << "sum is " << c;
+    
+    cout << endl << "bye, see you soon..." << endl;
+
+    return 0;
+}
